a493b6619ac3615170af11a0f77a046106f0c877
Leonid Kyryk <leonidkirik7777@gmail.com>
2025-03-21 19:41:42 +0000

    change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -41,7 +41,7 @@ import Product from './Product';
 export default function App() {
   return (
     <div>
-      <h1>Best selling</h1>
+      <h1>Best selling here</h1>
 
       <Product />
       <Product />
