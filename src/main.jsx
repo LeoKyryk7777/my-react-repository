9cd1576652ad4c112b703c04e2e08470b64e7d80
Leonid Kyryk <leonidkirik7777@gmail.com>
2025-03-21 19:29:58 +0000

    hw1

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './components/App'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
+function Product(params) {
+  return (
+    <dav>
+      <h1>cookies</h1>
+    </dav>
+  )
+  
+}
